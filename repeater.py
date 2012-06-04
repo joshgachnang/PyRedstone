249cfbec7f8e772c3114c4dc86f1d85774c54943
Josh Gachnang <Josh@ServerCobra.com>
2012-06-04 07:23:18 -0500

    get() over send() in repeater makes more sense to me and sounds correct when used in production code.

--- repeater.py
+++ repeater.py
@@ -13,7 +13,7 @@ class Repeater():
         self.headers['Content-Type'] = 'application/json'
         self.server_url = server_url
         self.username = username
-    def send(self, action, args):
+    def get(self, action, args):
         jdata = json.dumps({"action": action, "username": self.username, "auth_token": "", "args": args})
         req = urllib2.Request(self.server_url, jdata, self.headers)
         f = urllib2.urlopen(req)
