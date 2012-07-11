98142ba7f082eafd73394cf869ee41ec7a9b1404
Josh Gachnang <Josh@ServerCobra.com>
2012-07-11 14:52:07 -0500

    Starting testing suite.

---
+++ pyredstone/test/test_pyredstone.py
@@ -0,0 +1,26 @@
+import unittest
+
+class TestServerStatus(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_start(self):
+        pass
+
+    def test_start_again(self):
+        pass
+
+    def test_stop(self):
+        pass
+
+    def test_stop_again(self):
+        pass
+
+    def test_restart(self):
+        pass
+
+    def test_status(self):
+        pass
