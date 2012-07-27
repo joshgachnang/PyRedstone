5dcb12d410ce789bfb66cf3ba9fd1b03ba172d22
Josh Gachnang <Josh@ServerCobra.com>
2012-07-26 20:58:45 -0500

    Fixing error with single args

--- pyredstone/server.py
+++ pyredstone/server.py
@@ -151,6 +151,7 @@ class Root:
                         result = methodToCall(**args)
                     else:
                         result = methodToCall(args)
+
                 except AttributeError as e:
                     logger.error("Action %s not found." % action)
                     raise cherrypy.HTTPError(404, "Action %s not found." % action)
